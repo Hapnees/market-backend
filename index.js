9d18b5658acf27660e7f7deb2ad3b0a0d6fa8a2d
Hapnees <8dwardm8lton@gmail.com>
2023-03-26 02:10:14 +0300

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+import jsonServer from 'json-server'
+
+const PORT = 3001
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults({ static: './static' })
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(PORT, () => console.log(`JSON server is listening PORT ${PORT}`))
